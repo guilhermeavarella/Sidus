bc909d924dc93e51ce23bf866f113fc2fe4bc8e4
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-03-08 15:36:47 -0300

    feat: app icon component

---
+++ frontend/src/components/AppIcon.jsx
@@ -0,0 +1,26 @@
+import iconVoid from "/icons/app-void.svg"
+import icon from "/icons/app.svg"
+
+export default function AppIcon({ isActive = true, action, isLoaded = false }) {
+    if (!isActive) 
+        return(
+            <button 
+            className={`flex flex-col items-center justify-center transition-all duration-8000 delay-1000 ${
+                !isLoaded ? "blur-xs opacity-95" : "blur-md opacity-40"
+            }`}
+            >
+                <img src={iconVoid} alt="App" className="w-[6rem]" />
+                <p>Other</p>
+            </button>
+    )
+
+    return (
+        <button 
+        className="flex flex-col items-center justify-center drop-shadow-sm duration-500 ease-out hover:scale-110 active:scale-95 transition-all hover:cursor-pointer" 
+        onClick={action}
+        >
+            <img src={icon} alt="App" className="w-[6rem]" />
+            <p>Sidus</p>
+        </button>
+    )
+}
