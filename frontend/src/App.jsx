be28113030f629c37dd6e9e831062df7ca535a95
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-03-11 17:34:28 -0300

    feat: app nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
 import { Outlet } from "react-router-dom"
 
+import NavItem from "./components/NavItem.jsx"
+
 function App() {
+  const [activeTab, setActiveTab] = useState("home")
+
+  const navItems = ["Home", "Wallet", "Services", "Support"]
+
   return (
     <div className="w-full h-full flex items-start justify-center p-4 pb-24">
+      <section className="absolute bottom-16 flex items-center justify-center bg-white/25 backdrop-blur-sm rounded-full px-6 py-4 gap-9">
+        {navItems.map((item) => (
+          <NavItem key={item} label={item} isActive={activeTab === item.toLowerCase()} />
+        ))}
+      </section>
+
       <Outlet />
     </div>
   )
