24bfea2eea04f8a2c21c7319f813d0370505f8f5
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-03-11 17:43:07 -0300

    feat: nav item additional actions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className="w-full h-full flex items-start justify-center p-4 pb-24">
       <section className="absolute bottom-16 flex items-center justify-center bg-white/25 backdrop-blur-sm rounded-full px-6 py-4 gap-9">
         {navItems.map((item) => (
-          <NavItem key={item} label={item} isActive={activeTab === item.toLowerCase()} />
+          <NavItem key={item} label={item} isActive={activeTab === item.toLowerCase()} action={() => setActiveTab(item.toLowerCase())} />
         ))}
       </section>
 
