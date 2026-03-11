6dcb7dd2a24e6325d348c2d26524d26c3ba64575
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-03-11 17:29:34 -0300

    feat: nav item component

---
+++ frontend/src/components/NavItem.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom"
+
+function NavItem({ label, isActive}) {
+    const navigate = useNavigate();
+
+    return (
+        <button onClick={() => navigate(`/app/${label.toLowerCase()}`)} className="hover:cursor-pointer">
+            <img src={isActive ? `/icons/${label.toLowerCase()}-active.svg` : `/icons/${label.toLowerCase()}.svg`} alt={`${label}`} className="w-8 h-8" />
+        </button>
+    )
+}
+
+export default NavItem
