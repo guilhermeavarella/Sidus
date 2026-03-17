35acb20ef0733683ae662ee49125e9403c3ac5bb
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-03-17 09:28:18 -0300

    feat: home page

--- frontend/src/Emulator.jsx
+++ frontend/src/Emulator.jsx
@@ -7,7 +7,7 @@ function Emulator() {
   if (!isSmallScreen()) {
     return (
       <div className="relative w-[32rem]">
-        <div className="z-0 absolute inset-0 flex items-start justify-center overflow-x-hidden pt-[5.1rem] px-[0.68rem] rounded-[5.3rem] bg-[var(--background-primary)]"> {/* Tons of magic numbers because it looks better this way ;) */}
+        <div className="z-0 absolute inset-0 flex items-start justify-center overflow-x-hidden pt-[5.1rem] px-[0.68rem] rounded-[5.3rem] bg-[var(--background-primary)] no-scrollbar"> {/* Tons of magic numbers because it looks better this way ;) */}
           <Outlet />
         </div>
         <img src={frame} alt="Emulator Frame" className="z-10 relative w-full pointer-events-none" />
@@ -16,7 +16,7 @@ function Emulator() {
   }
   
   return (
-    <div className="w-[100dvw] h-[100dvh] flex items-start justify-center overflow-x-hidden overflow-y-scroll">
+    <div className="w-[100dvw] h-[100dvh] flex items-start justify-center overflow-x-hidden overflow-y-scroll bg-[var(--background-primary)] no-scrollbar">
       <Outlet />
     </div>
   )
