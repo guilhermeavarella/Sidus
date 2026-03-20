fe2227efa0f5ec5630de31d6864337f6639e282d
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-03-20 09:19:05 -0300

    fix: action button action

--- frontend/src/components/ActionButton.jsx
+++ frontend/src/components/ActionButton.jsx
@@ -1,20 +1,20 @@
 function ActionButton({ variant, action, label }) {
     if (variant === 'primary') {
         return (
-            <button className="h-12 px-8 bg-[var(--brand-secondary)] text-[var(--content-inverse)] font-semibold rounded-full gap-2 hover:cursor-pointer snap-start"> 
+            <button className="h-12 px-8 bg-[var(--brand-secondary)] text-[var(--content-inverse)] font-semibold rounded-full gap-2 hover:cursor-pointer snap-start" onClick={action}> 
                 {label}
             </button>
         );
     } else if (variant === 'selected') {
         return (
-            <button className="h-12 px-8 bg-[var(--background-secondary)] text-[var(--content-primary)] font-semibold rounded-full gap-2 hover:cursor-pointer snap-start border-3 border-[var(--brand-secondary)]"> 
+            <button className="h-12 px-8 bg-[var(--background-secondary)] text-[var(--content-primary)] font-semibold rounded-full gap-2 hover:cursor-pointer snap-start border-3 border-[var(--brand-secondary)]"onClick={action}> 
                 {label}
             </button>
         );
     }
 
     return (
-        <button className="h-12 px-8 bg-[var(--background-secondary)] text-[var(--content-primary)] font-semibold rounded-full gap-2 hover:cursor-pointer snap-start"> 
+        <button className="h-12 px-8 bg-[var(--background-secondary)] text-[var(--content-primary)] font-semibold rounded-full gap-2 hover:cursor-pointer snap-start" onClick={action}>
             {label}
         </button>
     );
